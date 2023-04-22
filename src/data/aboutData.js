800ff11058b452944fcdc68b01ed2d48147d0977
Andrew Bukenya <andrew.bukenya@bodastage.com>
2023-04-22 22:45:11 +0300

    Adjusted abt info

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name's Andrew. I'm an AI/ML Software Engineer and Data Scientist based in East-Africa.",
+    description2: "During the day I work as a Software Development Engineer at Bodastage Solutions and in the evening I  work on Research projects, schooling and personal studies to extend my knowledge base. I spend my leisure hours writing, reading, social meetings and volley. Currently I'm utilizing machine learning, AI and Data Science to optimize Healthcare services and general medicine. I'm passionate about exploring new areas.",
     image: 2
 }
