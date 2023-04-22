4e03a18203c1685009eb01a2ceba12cd5cc64ea5
Andrew Bukenya <andrew.bukenya@bodastage.com>
2023-04-22 22:53:18 +0300

    Readjusted abt info

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Andrew Bukenya',
     title: "AI/ML Software Engineer | Data Scientist",
-    desciption:""With a vision, Nothing is Impossible." It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    desciption:"With a vision, Nothing is Impossible. It was the clouds but soon reached and beyond",
     image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
     resumePdf: resume
 }
