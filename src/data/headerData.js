5a59bd4012df8eb08fb8780933d242ecac4db74d
Andrew Bukenya <andrew.bukenya@bodastage.com>
2023-04-23 03:08:32 +0300

    Changed headerdata, image folder

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
-import pflpic from '../assets/png/pflpic'
 
 export const headerData = {
     name: 'Andrew Bukenya',
     title: "AI/ML Software Engineer | Data Scientist",
     desciption:"With a vision, Nothing is Impossible. It was the clouds but soon reached and beyond",
-    image: pflpic, //'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://github.com/Andrkenya/pfl/blob/master/src/assets/png/pflpic.jpg', //'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
     resumePdf: resume
 }
