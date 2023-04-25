1a2ad2bdaa85f931a3dea4b91d5e977b0665496a
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-25 22:20:41 +0300

    Updated .about

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,8 +2,8 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Andrew Bukenya',
-    title: "AI/ML Software Engineer | Data Scientist",
-    desciption:"With a vision, Nothing is Impossible. It was the clouds but soon reached and beyond",
+    title: "AI/ML Engineer | SW-Engineer & Data Scientist",
+    desciption:"With a vision, Nothing is Impossible. Passionate about conceptualizing, building and developing novel approaches through embracing cutting-edge technology. Hence contributing to humanity, SDGs and the Encosystem.",
     image: 'https://raw.githubusercontent.com/Andrkenya/doc-s/main/pflpic.png',
     resumePdf: resume
 }
