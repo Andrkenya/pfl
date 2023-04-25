c43b64bfa5661094b2244774966d5179aebcfd67
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-25 22:54:31 +0300

    Added edc

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
+        institution: 'Makerere University',
+        course: 'Master of Science in Computer Science',
+        startYear: '2021',
+        endYear: 'Present'
     },
     {
         id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
+        institution: 'College',
+        course: 'Bachelor of Science in Computer Science',
+        startYear: '2016',
         endYear: '2019'
     },
     {
         id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
+        institution: 'Cisco Networking Academy',
+        course: 'Cisco Certified Network Associate',
+        startYear: '2016',
+        endYear: '2018'
     },
 ]
