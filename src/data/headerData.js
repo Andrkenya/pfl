d270fa464e571208507c190d564d308bd509bc34
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-25 22:00:08 +0300

    Changed header dt

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Andrew Bukenya',
     title: "AI/ML Software Engineer | Data Scientist",
     desciption:"With a vision, Nothing is Impossible. It was the clouds but soon reached and beyond",
-    image: 'https://github.com/Andrkenya/doc-s/blob/main/pflpic.png',
+    image: 'https://raw.githubusercontent.com/Andrkenya/doc-s/main/pflpic.png',
     resumePdf: resume
 }
