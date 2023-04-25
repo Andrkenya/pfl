e9bfa134f5ce42775401ebbebabf62b0fb901b34
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-25 23:23:57 +0300

    emplt changed

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,23 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Bodastage Solutions',
+        jobtitle: 'Software Development Engineer',
+        startYear: 'Jan, 2022',
+        endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'Aynwerd Inc.',
+        jobtitle: 'System Operations',
+        startYear: '2016',
+        endYear: '2021'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
-        endYear: 'Present'
+        company: 'IDI',
+        jobtitle: 'Trainee',
+        startYear: '2018',
+        endYear: '2018'
     },
 ]
