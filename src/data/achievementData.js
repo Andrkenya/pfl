af5b7a7e7f2528a235d317f3886cf651d6e3604f
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-26 00:19:04 +0300

    Update achievementData.js

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,5 +1,5 @@
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    bio : "Over the years, Andrew has fruitfully obtained several wins and certificates. *Putting quantified achievements on a resume is great, but I just don’t work with hard numbers* The quality of my work is auto-ticked.",
     achievements : [
         {
             id : 1,
