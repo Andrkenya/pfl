4afc86a95d46535cfe49ea1ad8f8ed66d1842d2d
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-28 00:55:23 +0300

    Changed blog

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -17,26 +17,26 @@ export const blogData = [
     },
     {
         id: 3,
-        title: 'BEING ANONYMOUS: A BEGINNERS GUIDE',
-        description: 'Here are some basic ways to lay off the Radar',
-        date: 'Aug 14, 2020',
-        image: 'https://1.bp.blogspot.com/-GR19yCNbKCE/XuRcKEfKvhI/AAAAAAAAAwY/srFWw5pOpzMibeJb__SmgZpx9dIpNNWqgCK4BGAsYHg/w640-h426/anonymous1.jpg',
+        title: 'Big Data Analytics',
+        description: 'With an increase in the volune of data collected by several sectors. Its hectic to quickly extract knowledge & insights which calls for "Analysts".',
+        date: 'Feb 5, 2023',
+        image: 'https://raw.githubusercontent.com/Andrkenya/doc-s/main/bgdata.png',
         url: '#'
     },
     {
         id: 4,
-        title: 'CHECK INTERNET SPEED FROM THE COMMAND LINE',
-        description: 'Want to check your internet speed from the terminal? Here\'s the perfect way to the that.',
-        date: 'Sep 17, 2020',
-        image: 'https://1.bp.blogspot.com/-XTVMsZP3HaM/Xxpl394txlI/AAAAAAAABws/zovXWiuNGg4TzBwYYVFfJ1ixDb3JfVokgCNcBGAsYHQ/w400-h195/speedtest.png',
+        title: '',
+        description: '',
+        date: '',
+        image: '',
         url: '#'
     },
     {
         id: 5,
-        title: 'GET MICHAEL\'S OR TREVOR\'S VEHICLES FULLY UPGRADED IN GTA V FOR FREE',
-        description: 'In case you haven\'t heard of this game, GTA stands for \'Grand Theft Auto\'. It is an open-world game published by Rockstar Games.',
-        date: 'Oct 1, 2020',
-        image: 'https://1.bp.blogspot.com/-jhdtUBIRD_s/XxLFKQuRz9I/AAAAAAAABsM/mClfbfDFKvsUwA7Wyi25Lzu3DEmL2lxrACNcBGAsYHQ/s1919/gta.jpg',
+        title: '',
+        description: '',
+        date: '',
+        image: '',
         url: '#'
     },
 ]
