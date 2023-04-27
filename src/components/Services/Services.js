c50b877fcf1f74b172ff5ec635ddaca7018a17cf
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-28 02:43:39 +0300

    Modifed Service

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -19,7 +19,7 @@ function Services() {
                     </div>
                     <div className="services-body">
                         <p style={{color:theme.tertiary80}}>
-                            These are some of the services I offer. Reach out to me if I can help you with any!
+                            He's got some practical hobbies. Reach out to him if want to share related concepts in these fields!
                         </p>
                         <div className="services-bodycontainer">
                             {servicesData.map(services => (
