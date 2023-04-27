0209db1614895e36457ee576719fc4a95d07668c
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-28 02:38:11 +0300

    Changed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,11 +18,11 @@ function Footer() {
     return (
         <div className="footer" style={{backgroundColor: theme.secondary}}>
             <p style={{color: theme.tertiary}}>
-                Made with  
+                2023 Designed with  
                 <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
                     ❤
                 </span>
-                 by {shortname(headerData.name)}
+                 | {shortname(headerData.name)}
             </p>
         </div>
     )
