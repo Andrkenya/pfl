c387aa9744160adb750c1eeeb143f0e8a7a323f5
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-28 03:34:20 +0300

    Updated educ to new

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,7 +1,7 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Exploring initial stages of genes & sequencing',
+        institution: 'Genes & Sequencing',
         course: 'Doctoral Prospective (Genomic Data Science & Machine Learning)',
         startYear: '2024',
         endYear: '2027'
