67ce0bb47c3b96157ad9e6480a28babd34d1d406
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-28 11:00:10 +0300

    Updated socials

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,8 +1,8 @@
 export const socialsData = {
-    github: 'https://github.com/',
+    github: 'github.com',
     linkedIn: 'https://www.linkedin.com/in',
-    twitter: 'https://twitter.com/Andrkenya',
-    medium: 'https://medium.com/@Andrew',
-    stackOverflow: 'https://stackoverflow.com/users/Andrew',
+    twitter: 'twitter.com',
+    medium: 'medium.com',
+    stackOverflow: 'https://stackoverflow.com',
     gitlab: 'https://gitlab.com/Andreya1000'
 }
