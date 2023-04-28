471ed51835bad97524f70d70eeffcb33b606c7ea
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-04-28 10:48:00 +0300

    Updated achivements

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -20,7 +20,7 @@ export const achievementData = {
         {
             id : 3,
             title : 'East African Illusional graphics',
-            details : 'Conceptualized a graphical illusion , transformed it into an idea and recognosed.',
+            details : 'Conceptualized a graphical illusion , transformed it into an idea.',
             date : 'Aug 20, 2013',
             field : 'Graphics',
             image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
