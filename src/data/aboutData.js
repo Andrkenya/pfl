93d1d5eb372475127368ae93743ec3e657ed9ed1
Andrew Bukenya <bukenyaandrew@outlook.com>
2023-05-26 18:16:44 +0300

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-    title: "Let me re-introduce Him",
-    description1: "Andrew is an AI/ML Specialist, a dedicated Software Engineering practitioner and a Data Scientist based in the Pearl of Africa.",
+    title: "About Him",
+    description1: "Andrew is an AI/ML Specialist, a dedicated Software Engineering practitioner and a Data Scientist based in Africa.",
     description2: "On a daily basis, he works as a Software Development Engineer at Bodastage Solutions and in the evening he focuses on his Research projects, school activities and personal studies to extend his expertise and knowledge base. Leisure hours are spent writing, reading, social gatherings and volley. Currently his research is focused on utilizing Deep learning and Data Science to optimize Healthcare services and general medicine. Additionally he's passionate about exploring new fields and inventions globally.",
     image: 2
 }
